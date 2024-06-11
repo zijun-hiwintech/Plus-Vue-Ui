313dd96d13ac742a6331af53c03a130ba5bca47f
zijun-hiwintech <zijun@hiwintech.com>
2024-06-11 10:08:40 +0800

    init data

---
+++ src/types/env.d.ts
@@ -0,0 +1,27 @@
+declare module '*.vue' {
+  import { DefineComponent } from 'vue';
+  const Component: DefineComponent<{}, {}, any>;
+  export default Component;
+}
+
+// 环境变量
+interface ImportMetaEnv {
+  VITE_APP_TITLE: string;
+  VITE_APP_PORT: number;
+  VITE_APP_BASE_API: string;
+  VITE_APP_BASE_URL: string;
+  VITE_APP_CONTEXT_PATH: string;
+  VITE_APP_MONITRO_ADMIN: string;
+  VITE_APP_POWERJOB_ADMIN: string;
+  VITE_APP_EASYRETRY_ADMIN: string;
+  VITE_APP_ENV: string;
+  VITE_APP_ENCRYPT: string;
+  VITE_APP_RSA_PUBLIC_KEY: string;
+  VITE_APP_RSA_PRIVATE_KEY: string;
+  VITE_APP_CLIENT_ID: string;
+  VITE_APP_WEBSOCKET: string;
+}
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+  // readonly glob: any;
+}
